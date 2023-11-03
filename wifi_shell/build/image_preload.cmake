be3f3e9807798cb669f153b4ca57d0d3c9e8de65
ehughes <ehughes@wavenumber.net>
2023-11-02 22:01:36 -0400

    change shell to wifi_shell

---
+++ wifi_shell/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "rt_super" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/ELI/rt_super_z/rt_super_z/wifi_shell/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(SHIELD "nrf7002ek" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/Python311/python.exe" CACHE INTERNAL "NCS child image controlled")
